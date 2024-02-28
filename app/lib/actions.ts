4d221fb5247433eb9b2c7f8e002ae9f4b83c62b7
Pavlo Kreitsberg <pavel.kreitsberg@gmail.com>
2024-02-28 15:37:58 +0200

    end course

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,10 +1,11 @@
 'use server';
 
 import { sql } from "@vercel/postgres";
-import { log } from "console";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 import { z } from "zod";
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
 
 export type State = {
   errors?: {
@@ -109,4 +110,24 @@ export async function deleteInvoice(id: string) {
     } catch (error) {
         return { message: 'Database Error: Failed to Delete Invoice.' };        
     }  
+}
+
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw error;
+  }
 }
